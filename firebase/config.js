2a7555e2e8244192c7e953f5ffc7b4e48ddf7e62
Tanner <tannerbouteiller@gmail.com>
2021-10-09 19:08:01 -0500

    initalized firebase and deployed app

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API,
+  authDomain: process.env.REACT_APP_FB_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT,
+  storageBucket: process.env.REACT_APP_FB_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_SENDER,
+  appId: process.env.REACT_APP_FB_APP,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
